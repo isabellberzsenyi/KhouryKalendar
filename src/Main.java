981d3424ce50dfaf65c77293441e1d96578756c5
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2020-03-23 19:03:03 -0400

    main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+import javax.swing.*;
+
 import Views.ViewMain;
 
 public class Main {
   public static void main(String[] args) {
+    try {
+      //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+      for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+        if ("Nimbus".equals(info.getName())) {
+          UIManager.setLookAndFeel(info.getClassName());
+          break;
+        }
+      }
+    } catch (UnsupportedOperationException e) {
+      e.printStackTrace();
+    }
+    catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (InstantiationException e) {
+      e.printStackTrace();
+    } catch (UnsupportedLookAndFeelException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
     ViewMain v = new ViewMain();
   }
 }
